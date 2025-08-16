88b5fd247e213e626f7095c0809f682485ee8e94
GuillermoSM5 <guillermosotomarquez5@gmail.com>
2025-08-15 18:30:37 -0600

    feat(header): add one part for header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,11 +3,11 @@ import { AiFillSun } from "react-icons/ai";
 const Header = () => {
     return (<div className="border-b border-border">
         <div className=" m-auto container p-7 flex justify-between">
-            <p className="font-extrabold text-xl font-mont">
+            <p className="font-extrabold text-xl font-mont flex items-center">
                 Guillermo Soto
             </p>
-            <div className="flex items-center p-2 rounded-xl hover:text-(--color-background) hover:bg-(--primary-color)   cursor-pointer">
-                <AiFillSun className="text-xl" />
+            <div className="flex items-center p-3 rounded-xl hover:text-(--color-background) hover:bg-(--primary-color)   cursor-pointer">
+                <AiFillSun className="text-lg" />
             </div>
         </div>
     </div>);
